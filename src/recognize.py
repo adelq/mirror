8c5c1ad4f2787f3621acfdea1cea4862e7b758f9
Greg Terrono <terronogr@gmail.com>
2015-04-14 18:05:29 -0400

    Recognizes the word weather

--- src/recognize.py
+++ src/recognize.py
@@ -1,9 +1,17 @@
 import speech_recognition as sr
+import re
+from modules.weather import weather
+
+
 def callback(recognizer, audio):                          # this is called from the background thread
     print("trying to recognize")
     try:
-        print(recognizer.recognize(audio))  # received audio data, now need to recognize it
+        text = recognizer.recognize(audio)  # received audio data, now need to recognize it
+        print(text)
+        if re.search("weather", text):
+            print(weather.get_weather())
     except LookupError:
+        print('error')
         pass
 r = sr.Recognizer()
 r.listen_in_background(sr.Microphone(), callback)
